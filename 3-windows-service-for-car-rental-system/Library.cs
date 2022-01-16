8b11f9e0928bbd307b4612709c4b091ec1930a41
GunaRakulan-GR <gunarakulan@gmail.com>
2022-01-16 14:56:36 +0530

    Create windows service for car rental system

---
+++ 3-windows-service-for-car-rental-system/Library.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestWindowService
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                sw = new StreamWriter("C:\\CRMS\\CRMSNetworkSystem.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                sw = new StreamWriter("C:\\CRMSNetworkSystem.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ 3-windows-service-for-car-rental-system/Scheduler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TestWindowService
+{
+    public partial class Scheduler : ServiceBase
+    {
+        private Timer timer1 = null;
+
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1 = new Timer();
+            this.timer1.Interval = 5000; //every 10 secs
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Tick);
+            timer1.Enabled = true;
+            Library.WriteErrorLog("The CRMS Online system is started");
+        }
+
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            //Write code here to do some job depends on your requirement
+            Library.WriteErrorLog("Searching for the specified server IP 198.67.36.55");
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+            Library.WriteErrorLog("The server is not found Network error 404!");
+        }
+    }
+}
